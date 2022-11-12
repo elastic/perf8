2f1ff17a7e384d1c404937f0f6cdac9c10535e91
Tarek Ziadé <tarek@ziade.org>
2022-11-11 19:45:24 -0800

    put generate_plot in the class

--- perf8/_psutil.py
+++ perf8/_psutil.py
@@ -4,40 +4,39 @@ import matplotlib.pyplot as plt
 import time
 
 
-def generate_plot(path):
-    x = []
-    rss = []
-    # cpu = []
-
-    with open(path) as csvfile:
-        lines = csv.reader(csvfile, delimiter=",")
-        for i, row in enumerate(lines):
-            if i == 0:
-                continue
-            x.append(row[-1])
-            bytes = round(int(row[0]) / (1024 * 1024), 2)
-            rss.append(bytes)  # rss
-            # cpu.append(row[-1])
-
-    # plt.plot(x, cpu, color="r", linestyle="dashed", marker="o", label="CPU %")
-    plt.plot(x, rss, color="g", linestyle="dashed", marker="o", label="RSS")
-
-    plt.xticks(rotation=25)
-    plt.xlabel("Duration")
-    plt.ylabel("Bytes")  # switch to KiB or MiB automatically - XXX
-    plt.title("Performance Report", fontsize=20)
-    plt.grid()
-    plt.legend()
-    plt.savefig("report.png")
-    return "report.png"
-
-
 class ResourceWatcher:
     name = "psutil"
 
     def __init__(self, **options):
         self.report_fd = self.writer = self.proc_info = None
         self.report_file = options.get("report_file", "report.csv")
+
+    def generate_plot(self, path):
+        x = []
+        rss = []
+        # cpu = []
+
+        with open(path) as csvfile:
+            lines = csv.reader(csvfile, delimiter=",")
+            for i, row in enumerate(lines):
+                if i == 0:
+                    continue
+                x.append(row[-1])
+                bytes = round(int(row[0]) / (1024 * 1024), 2)
+                rss.append(bytes)  # rss
+                # cpu.append(row[-1])
+
+        # plt.plot(x, cpu, color="r", linestyle="dashed", marker="o", label="CPU %")
+        plt.plot(x, rss, color="g", linestyle="dashed", marker="o", label="RSS")
+
+        plt.xticks(rotation=25)
+        plt.xlabel("Duration")
+        plt.ylabel("Bytes")  # switch to KiB or MiB automatically - XXX
+        plt.title("Performance Report", fontsize=20)
+        plt.grid()
+        plt.legend()
+        plt.savefig("report.png")
+        return "report.png"
 
     def start(self, pid):
         self.proc_info = psutil.Process(pid)
@@ -79,6 +78,6 @@ class ResourceWatcher:
     def stop(self, pid):
         if self.report_fd is not None:
             self.report_fd.close()
-            plot_file = generate_plot(self.report_file)
+            plot_file = self.generate_plot(self.report_file)
             return [plot_file, self.report_file]
         return []
