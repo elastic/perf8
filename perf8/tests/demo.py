408b4a352814525e70569f92b9a538e2f768dd4a
Tarek Ziadé <tarek@ziade.org>
2022-11-12 16:41:29 -0800

    tweak demo

--- perf8/tests/demo.py
+++ perf8/tests/demo.py
@@ -4,18 +4,24 @@ Demo script to test perf8
 import random
 
 data = []
+RANGE = 100000
 
 
 def get_random(min, max):
     return random.randint(min, max)
 
+def do_math(x, y):
+    x * y
+    y ** x
+
 
 def main():
-    for i in range(4000):
+    for i in range(RANGE):
         data.append(get_random(1, 3722837687624) ** 4)
         data.append("r" * get_random(10000, 2000000))
         if get_random(1, 20) == 2:
-            print(f"Busy adding data! ({i}/4000)")
+            print(f"Busy adding data! ({i}/{RANGE})")
+            do_math(i, RANGE)
 
 
 if __name__ == "__main__":
