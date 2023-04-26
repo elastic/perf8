16bfe78444bc7aabaf236660c465959ef10fda15
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2023-04-26 18:56:13 +0200

    Maybe like this?

--- perf8/plugins/base.py
+++ perf8/plugins/base.py
@@ -223,13 +223,15 @@ class BasePlugin:
 
         plt.xticks(rotation=25)
         plt.xlabel("Duration (s)")
-        xtick_step = int(len(x) / 10) if len(x) > 9 else 1
-        existing_ticks = ax.get_xticks()
-        ticks = existing_ticks[::xtick_step]
-        # If last tick is missing, add it!
-        if ticks[-1] != x[-1]:
-            ticks.append(existing_ticks[-1])
-        ax.set_xticks(ticks)
+
+        if x:
+            xtick_step = int(len(x) / 10) if len(x) > 9 else 1
+            existing_ticks = ax.get_xticks()
+            ticks = existing_ticks[::xtick_step]
+            # If last tick is missing, add it!
+            if ticks[-1] != x[-1]:
+                ticks.append(existing_ticks[-1])
+            ax.set_xticks(ticks)
 
         plt.ylabel(ylabel)
         if yformatter:
